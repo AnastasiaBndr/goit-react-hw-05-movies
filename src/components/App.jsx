6f400ba53a0f26bfd3d70c729a30b064c3c44184
Anastasiia-Bondarchuk <125996065+AnastasiaBndr@users.noreply.github.com>
2023-09-27 18:19:20 +0300

    updt

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -49,7 +49,7 @@ export default function App() {
 
   return (<Routes>
     <Route path='/' element={<Header />}>
-      <Route path="movies" element={<MovieList movies={movies} click={onClickMovie} loadMore={handleLoadMore} />} />
+      <Route path="goit-react-hw-05-movies" element={<MovieList movies={movies} click={onClickMovie} loadMore={handleLoadMore} />} />
       <Route path={currentMovie.id + ""} element={<CurrentMoviePage movie={currentMovie} />}>
         <Route path={'cast'} element={<Cast movie={currentMovie} apiComponent={apiComponent} />} />
         <Route path={'fgfg'} element={<></>} />
