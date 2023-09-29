a052361935a8ea23f1aa31d58689d180fc8672e0
AnastasiaBndr <abondarchuk1149@gmail.com>
2023-09-29 17:57:18 +0300

    hh

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { ApiComponent } from 'apiComponent';
 import { Route, Routes } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import { CirclesWithBar } from 'react-loader-spinner';
+
 import Header from 'Header';
 import MovieList from 'MovieList';
-import CurrentMoviePage from 'CurrentMoviePage';
 import Cast from 'Cast';
 import Reviews from 'Reviews';
 import Search from 'Search';
 
 const apiComponent = new ApiComponent();
+const CurrentMoviePageLazy=lazy(()=>import("CurrentMoviePage"));
 
 export default function App() {
 
@@ -112,21 +115,38 @@ export default function App() {
 
   }
 
-  return (<Routes>
+  return (<Suspense fallback={<CirclesWithBar
+    height="100"
+    width="100"
+    color="#4fa94d"
+    wrapperStyle={{}}
+    wrapperClass=""
+    visible={true}
+    outerCircleColor=""
+    innerCircleColor=""
+    barColor=""
+    ariaLabel='circles-with-bar-loading'
+    position="absolute"
+    top= "50%"
+    left="50%"
+    style={{ transform: 'translate(-50%, -50%)' }}
+  />}>
+
+  <Routes>
     <Route path='/' element={<Header />}>
       <Route path="home" element={<MovieList movies={trandingMovies} click={onClickMovie} loadMoreIsVisible={loadMoreIsVisible} loadMore={handleLoadMore} />} />
       <Route path="search" element={<Search movies={movies} onClickSubmit={onClickSubmit} click={onClickMovie} loadMore={handleLoadMoreForSearch} loadMoreIsVisible={loadMoreIsVisible} query={evt => setQuery(evt.target.value)} />}></Route>
-      <Route path='search/:id' element={<CurrentMoviePage movie={currentMovie} />}>
+      <Route path='search/:id' element={<CurrentMoviePageLazy movie={currentMovie} />}>
         <Route path={'cast'} element={<Cast movie={currentMovie} apiComponent={apiComponent} />} />
         <Route path={'reviews'} element={<Reviews movie={currentMovie} apiComponent={apiComponent} />} />
       </Route>
-      <Route path='home/:id' element={<CurrentMoviePage movie={currentMovie} />}>
+      <Route path='home/:id' element={<CurrentMoviePageLazy movie={currentMovie} />}>
         <Route path={'cast'} element={<Cast movie={currentMovie} apiComponent={apiComponent} />} />
         <Route path={'reviews'} element={<Reviews movie={currentMovie} apiComponent={apiComponent} />} />
       </Route>
     </Route>
 
-  </Routes>
+  </Routes></Suspense>
   );
 
 }
